5f77b0bd697a6f941bf48424a6f4677b841c365d
AnhDao19112001 <phantaanhdao@gmail.com>
2023-08-23 10:44:35 +0700

    fixed exercise_2.1

--- exercise/exercise_2/exercise_2.1/src/App.js
+++ exercise/exercise_2/exercise_2.1/src/App.js
@@ -1,6 +1,6 @@
 // import './App.css';
 import './StudentCSS/Student.css'
-import {students} from "./studentComponent/Student";
+import {students} from "./data/Student";
 
 export default function App() {
     return (
@@ -12,13 +12,13 @@ export default function App() {
                 <td>country</td>
             </tr>
             {
-                students.map(student => (
-                    <tr>
-                        <td>{student.company}</td>
-                        <td>{student.contact}</td>
-                        <td>{student.country}</td>
-                    </tr>
-                ))
+                students.map((element, key) => {
+                    return (<tr key={key + 1}>
+                        <td>{element.company}</td>
+                        <td>{element.contact}</td>
+                        <td>{element.country}</td>
+                    </tr>)
+                })
             }
         </table>
     );
